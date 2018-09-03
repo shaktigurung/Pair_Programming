0dc0c43e7cee50fe209e169a2eaf3edc86a42019
shakti gurung <mr.shakti.gurung@gmail.com>
2018-09-03 15:43:28 +1000

    Second commit

--- country.rb
+++ country.rb
@@ -3,8 +3,8 @@ require 'iso_country_codes'
 class Country
     attr_reader :country_name, :name , :continent , :calling_code, :phone_number, :image
     def initialize
-         @country_name = Faker::Address.country_code
-         country = @country_name
+        @country_name = Faker::Address.country_code
+        country = @country_name
         code = IsoCountryCodes.find(country)
         @name = code.name
         @continent = code.continent
@@ -14,13 +14,23 @@ class Country
     @country =[]
     def display
         puts "This is country : #{@country_name}"
-        puts "This is another country : #{@name}"
+        puts "Country Name : #{@name}"
         puts "Continent is: #{@continent}"        
         puts "Country calling code is: #{@calling_code}"
         puts "ISO code is: #{@numeric} "
         puts "***********************************"
     end
 end
+puts "*********************************"
+puts " Welcome to the Country Info "
+puts "*********************************"
+
+# puts " Enter the country name "
+# country = gets.chomp.to_s
+# country_info = []
+# country_info << Country.new
+# country_info.display
+
 ######### Creates country_info array and fills it with country info #######
 country_info = []
 for i in (0..5) do
